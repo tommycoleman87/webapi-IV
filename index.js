b159187741d9cc3b843e036de29a020c1b9942a5
Tommy Coleman <tommiilee@gmail.com>
2019-09-05 12:41:16 -0400

    added devlopment environment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('./api/server.js');
-
+require('dotenv').config();
 const port = 4000;
+console.log('environment', process.env.NODE_ENV)
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
